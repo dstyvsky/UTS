488a5c6becb191c7fce48031c56f7dd8d42a3e0a
Fyodor <dstyvsky@gmail.com>
2015-11-20 11:30:14 -0500

    added auto scroll

--- app/assets/javascripts/highlight.js
+++ app/assets/javascripts/highlight.js
@@ -6,7 +6,14 @@ $( document ).ready(function() {
   	$( this ).removeClass('service_photos');
   	$( this ).addClass( 'selected_service_photo');
 
-
-
+  	$('html, body').animate({
+        scrollTop: $("#header_container").offset().top
+    }, 2000);
   })
+
+  $('#services_button').click(function(){
+	  $('html, body').animate({
+	  	   scrollTop: $('#header_container').offset().top
+	  	}, 2000);
+	})
 });
