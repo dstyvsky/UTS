6a4ad6bf815f48941767ec5d0baab79a03bd6760
Fyodor <dstyvsky@gmail.com>
2015-11-30 10:00:26 -0500

    progress on sign in page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+
   root                     'static_pages#services'
-  get   'contacts'     =>  'contacts#new'
-  get   'recent_news'  =>  'static_pages#recent_news'
-  get   'about_us'     =>  'static_pages#about_us'
-  get   'location'     =>  'static_pages#location'
+  get    'contacts'     =>  'contacts#new'
+  get    'recent_news'  =>  'static_pages#recent_news'
+  get    'about_us'     =>  'static_pages#about_us'
+  get    'location'     =>  'static_pages#location'
+
+  get    'login'        =>  'sessions#new'
+  post   'login'        =>  'sessions#create'
+  delete 'logout'       =>  'sessions#destroy'
+
 
   resources "contacts", only: [:new, :create]
   # The priority is based upon order of creation: first created -> highest priority.
