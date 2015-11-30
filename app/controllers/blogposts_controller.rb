b54c39f163b0b4e5db2e00e5fca2616026dc64be
Fyodor <dstyvsky@gmail.com>
2015-11-30 11:36:02 -0500

    Able to delete posts

--- app/controllers/blogposts_controller.rb
+++ app/controllers/blogposts_controller.rb
@@ -1,5 +1,6 @@
 class BlogpostsController < ApplicationController
 	before_action :logged_in_user, only: [:create, :destroy]
+	before_action :correct_user, only: [:destroy]
 
 	def create
 		@blogpost = current_user.blogposts.build(blogpost_params)
@@ -12,11 +13,19 @@ class BlogpostsController < ApplicationController
 	end
 
 	def destroy
+		@blogpost.destroy
+		flash[:success] = "Blogpost deleted"
+		redirect_to recent_news_path
 	end
 
 	private
 
 		def blogpost_params
 			params.require(:blogpost).permit(:content)
+		end
+
+		def correct_user
+			@blogpost = current_user.blogposts.find_by(id: params[:id])
+			redirect_to recent_news_path if @blogpost.nil?
 		end
 end
