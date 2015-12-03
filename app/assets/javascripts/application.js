dde2da2e824367638adf484fc4160f6304117132
Fyodor <dstyvsky@gmail.com>
2015-12-02 22:41:31 -0500

    Second Try

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.turbolinks
-//= require ./readmore.js
-
 //= require_tree .
 
 //= require turbolinks
--- app/assets/javascripts/highlight.js
+++ app/assets/javascripts/highlight.js
@@ -51,8 +51,6 @@ $( document ).ready(function() {
   })
 
 
-  $('.more').readmore()
-
 
  
 });
