832a0698b27ee2570482286c5d3a95112c0deaed
Fyodor <dstyvsky@gmail.com>
2015-12-02 22:56:52 -0500

    maybe work

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ gem 'bcrypt'
 
 gem 'jquery-turbolinks'
 
+gem 'readmorejs-rails'
+
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 //= require jquery
 //= require jquery_ujs
 //= require jquery.turbolinks
 //= require_tree .
+
 
 //= require turbolinks
